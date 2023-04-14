4b2f1b6e6073ebae440f0f182fc8056a922f3d54
BatteredBunny <ayes2022@protonmail.com>
2023-04-14 14:01:40 +0300

    Add extension to navbar by default, add scripts for running and building extension

--- build_native_manifest.js
+++ build_native_manifest.js
@@ -1,7 +1,11 @@
-{
+const fs = require('fs');
+
+let manifest ={
     "name": "browser_terminal",
     "description": "Allows you to open a native shell in the browser",
-    "path": "/home/lain/Documents/native-shell-browser-extension/native/app.py",
+    "path": __dirname + "/native/app.py",
     "type": "stdio",
     "allowed_extensions": [ "browser_terminal@example.org" ]
 }
+
+fs.writeFileSync(manifest.name + '.json', JSON.stringify(manifest));
